e5d31d4c8cf9597766747a9d9cbb2cf9d52c277f
DeepanB2005 <deepanb2005@gmail.com>
2025-07-14 19:06:32 +0530

    recursion 1 to n

---
+++ recursion_basics/Nto0ne.java
@@ -0,0 +1,14 @@
+class NtoOne
+{
+    public static void main(String[] args) {
+        rec(5);
+    }
+    static void rec(int n)
+    {
+        if(n==0)
+        {
+            return;
+        }
+
+    }
+}
