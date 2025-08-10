761c15fbbc70fcdaaed7367500b15bd85a904fe7
DeepanB2005 <deepanb2005@gmail.com>
2025-08-10 20:35:15 +0530

    hashmap implementation

--- basic_hashing/hashmap.java
+++ basic_hashing/hashmap.java
@@ -1,5 +1,20 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Scanner;
 class hashmap
 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            map.put(a[i],i);
+        }
+        System.out.print(map);
+
+    }
 }
