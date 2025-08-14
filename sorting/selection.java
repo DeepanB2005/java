7eadfd017ee6c02adf50009240a9df3541f46cee
DeepanB2005 <deepanb2005@gmail.com>
2025-08-14 19:50:34 +0530

    sorting basics syntax

---
+++ sorting/selection.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class selenagomez
+{
+    public static void main(String []args)
+    {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        //selection
+        for(int i=0;i<n-1;i++)
+        {
+            int min=i;
+            for(int j=i;j<n;j++)
+            {
+                min=a[j]<a[min]?j:min;
+            }
+            int t=a[i];
+            a[i]=a[min];
+            a[min]=t;
+        }
+        for(int i:a)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
